32daae6920dea46e1fa1f08f11159ae61d4e3859
Sasha <ponomarek91@gmail.com>
2020-04-11 22:01:22 +0300

    Updates

---
+++ precache-manifest.4171c062a99a070edfcb303aa076e5fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e6fe445047ff2bb8d65b1b01663bd1",
+    "url": "/19HW-Cursor/index.html"
+  },
+  {
+    "revision": "e7a6e82880e67dfb06ea",
+    "url": "/19HW-Cursor/static/css/main.789f2349.chunk.css"
+  },
+  {
+    "revision": "5b317f5e59865b21cb63",
+    "url": "/19HW-Cursor/static/js/2.4984f772.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/19HW-Cursor/static/js/2.4984f772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a6e82880e67dfb06ea",
+    "url": "/19HW-Cursor/static/js/main.ce68c9bc.chunk.js"
+  },
+  {
+    "revision": "66cc7a6241d9eef86636",
+    "url": "/19HW-Cursor/static/js/runtime-main.a0324320.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/19HW-Cursor/static/media/padlock.d964e0b3.svg"
+  }
+]);
